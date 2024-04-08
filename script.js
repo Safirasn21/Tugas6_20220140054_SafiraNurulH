344a38f2ee1bea71c72914006bb8d24276f50ba6
safirasn21 <safirasn021@gmai.com>
2024-04-08 21:40:26 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("Video").addEventListener("click", function() {
+    alert("ini video BOYNEXTDOOR");
+});
+
+document.getElementById("Audio").addEventListener("click", function() {
+    alert("ini audio BOYNEXTDOOR");
+});
+
+document.getElementById("Gambar").addEventListener("click", function() {
+    alert("ini Gambar BOYNEXTDOOR");
+});
